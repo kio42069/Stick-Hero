305a6a612d0d5426b1da7cdf0f65fa8e78ef2c71
Rachit Arora <71589920+rcht@users.noreply.github.com>
2023-11-19 17:42:27 +0530

    main menu done

--- src/main/java/com/example/demo2/Main.java
+++ src/main/java/com/example/demo2/Main.java
@@ -20,16 +20,18 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Group root = new Group();
-        Scene scene = new Scene(root, 540, 966, Color.LIGHTSKYBLUE);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 540, 966, Color.LIGHTSKYBLUE);
 
 //        Image icon = new Image("");
 //        stage.getIcons().add(icon);
 
-        stage.setTitle("Stick Hero");
+//        stage.setTitle("Stick Hero");
 //        stage.setWidth(900);
 //        stage.setHeight(900);
-        stage.setResizable(false);
+//        stage.setResizable(false);
 
 //        stage.setFullScreen(true);
 //        stage.setFullScreenExitHint("wow lol, oops ho gaya");
@@ -37,37 +39,37 @@ public class Main extends Application {
 
 
         // TEXT
-        Text titleText = new Text();
-        titleText.setFont(Font.font("Roboto Mono", 50));
-        titleText.setText("Stick Hero");
-        titleText.setY(100);
-        titleText.setX(100);
-        root.getChildren().add(titleText);
+//        Text titleText = new Text();
+//        titleText.setFont(Font.font("Roboto Mono", 50));
+//        titleText.setText("Stick Hero");
+//        titleText.setY(100);
+//        titleText.setX(100);
+//        root.getChildren().add(titleText);
 
         // LINES
-        Line line = new Line();
-        line.setStartX(0);
-        line.setStartY(150);
-        line.setEndX(966);
-        line.setEndY(300);
-        line.setStrokeWidth(5);
-        root.getChildren().add(line);
+//        Line line = new Line();
+//        line.setStartX(0);
+//        line.setStartY(150);
+//        line.setEndX(966);
+//        line.setEndY(300);
+//        line.setStrokeWidth(5);
+//        root.getChildren().add(line);
 
         // IMAGES
-        Image image = new Image(
-                "https://wiki.gentoo.org/images/thumb/b/b8/Larry-nefarius-v2.svg/300px-Larry-nefarius-v2.svg.png"
-        );
-        ImageView imageView = new ImageView(image);
-        imageView.setX(200);
-        imageView.setY(300);
-        root.getChildren().add(imageView);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(180);
-        rectangle.setY(666);
-        rectangle.setWidth(180);
-        rectangle.setHeight(300);
-        root.getChildren().add(rectangle);
+//        Image image = new Image(
+//                "https://wiki.gentoo.org/images/thumb/b/b8/Larry-nefarius-v2.svg/300px-Larry-nefarius-v2.svg.png"
+//        );
+//        ImageView imageView = new ImageView(image);
+//        imageView.setX(200);
+//        imageView.setY(300);
+//        root.getChildren().add(imageView);
+//
+//        Rectangle rectangle = new Rectangle();
+//        rectangle.setX(180);
+//        rectangle.setY(666);
+//        rectangle.setWidth(180);
+//        rectangle.setHeight(300);
+//        root.getChildren().add(rectangle);
 
 
         stage.setScene(scene);
--- src/main/java/com/example/demo2/Menu.java
+++ src/main/java/com/example/demo2/Menu.java
@@ -2,13 +2,24 @@ package com.example.demo2;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Menu {
     @FXML
     private Label welcomeText;
+    @FXML
+    private javafx.scene.control.Button closeButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onPlayButtonClick() {
+//        System.out.println("it works??");
+//        welcomeText.setText("Welcome to JavaFX Application!");
+
+    }
+
+    @FXML
+    protected void onExitButtonClick() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 }
