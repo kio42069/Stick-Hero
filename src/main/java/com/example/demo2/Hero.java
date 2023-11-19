8929c54f4387683c421b54e6dd63b280dcb9f19c
Rachit Arora <71589920+rcht@users.noreply.github.com>
2023-11-19 20:29:15 +0530

    cums

---
+++ src/main/java/com/example/demo2/Hero.java
@@ -0,0 +1,10 @@
+package com.example.demo2;
+
+public class Hero {
+    private double xCoord;
+    private double yCoord;
+
+    public void move(){}
+    public void fall(){}
+    public void grabApple(){}
+}
